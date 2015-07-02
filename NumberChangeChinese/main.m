699064ddf5fec763cbffd81aed2be313a06fecc1
keyzhang <843387173@qq.com>
2015-07-02 11:23:52 +0800

    Initial Commit

---
+++ NumberChangeChinese/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NumberChangeChinese
+//
+//  Created by keyzhang on 15/7/2.
+//  Copyright © 2015年 keyzhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
